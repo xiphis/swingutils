38c1f8fe2a29ce294cdc015fbeceabec8bcf5c51
A T Curtis <atcurtis@gmail.com>
2023-04-17 18:29:53 -0700

    add COPYRIGHT

--- src/main/java/org/xiphis/swing/intern/PatternFormatter.java
+++ src/main/java/org/xiphis/swing/intern/PatternFormatter.java
@@ -1,4 +1,32 @@
 package org.xiphis.swing.intern;
+/*
+Copyright 2023 Xiphis and A. T. Curtis
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 import javax.swing.text.DefaultFormatter;
 import java.text.ParseException;
@@ -10,7 +38,7 @@ public class PatternFormatter extends DefaultFormatter {
     public static final Pattern EMAIL_PATTERN = Pattern.compile("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}");
 
     private Pattern pattern;
-    private transient Matcher matcher;
+    protected transient Matcher matcher;
 
     public PatternFormatter() {
 
@@ -28,22 +56,10 @@ public class PatternFormatter extends DefaultFormatter {
 
     public void setPattern(Pattern pattern) {
         this.pattern = pattern;
-        this.matcher = null;
     }
 
     public Pattern getPattern() {
         return pattern;
-    }
-
-    protected void setMatcher(Matcher matcher) {
-        if (matcher.pattern() != getPattern()) {
-            throw new IllegalArgumentException();
-        }
-        this.matcher = matcher;
-    }
-
-    protected Matcher getMatcher() {
-        return matcher;
     }
 
     /**
@@ -68,7 +84,7 @@ public class PatternFormatter extends DefaultFormatter {
             Matcher matcher = pattern.matcher(string);
 
             if (matcher.matches()) {
-                setMatcher(matcher);
+                this.matcher = matcher;
                 return super.stringToValue(string);
             }
             throw new ParseException("Pattern does not match", 0);
